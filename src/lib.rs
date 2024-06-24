5c33b662bed37f84c48535e2e856e4fd8e01b3ab
Nate River <trishiragolder69@outlook.com>
2024-06-25 04:40:43 +0530

    implemented basic arith-ops for Vec3

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,15 @@
 
+use std::ops::Neg;
 
 
+
+#[vector_xd_macro::derive_arith]
 pub struct Vec3 {
   pub x: f32,
   pub y: f32,
   pub z: f32
 }
+
 
 impl Vec3 {
   pub const fn new(x: f32,y: f32,z: f32)-> Self {
@@ -14,6 +18,17 @@ impl Vec3 {
 }
 
 
+impl Neg for Vec3 {
+  type Output=Vec3;
+
+  fn neg(self)-> Self::Output {
+    Vec3 {
+      x: -self.x,
+      y: -self.y,
+      z: -self.z
+    }
+  }
+}
 
 
 
