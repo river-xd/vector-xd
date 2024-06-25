259ac3d37597015c00e60a8a80898ba1dcf63b56
Nate River <trishiragolder69@outlook.com>
2024-06-25 19:51:19 +0530

    implemented constructing functions for Vec3

--- src/lib.rs
+++ src/lib.rs
@@ -3,6 +3,7 @@ use std::ops::Neg;
 
 
 
+#[derive(Debug,Clone,Copy,PartialEq)]
 #[vector_xd_macro::derive_arith]
 pub struct Vec3 {
   pub x: f32,
@@ -15,6 +16,45 @@ impl Vec3 {
   #[inline(always)]
   pub const fn new(x: f32,y: f32,z: f32)-> Self {
     Vec3 { x,y,z }
+  }
+
+  #[inline]
+  pub const fn splat(v: f32)-> Self {
+    Vec3 {
+      x: v,
+      y: v,
+      z: v
+    }
+  }
+
+  #[inline]
+  pub const fn from_array(v: [f32;3])-> Self {
+    Vec3 {
+      x: v[0],
+      y: v[1],
+      z: v[2]
+    }
+  }
+
+  #[inline]
+  pub const fn to_array(self)-> [f32;3] {
+    [self.x,self.y,self.z]
+  }
+
+  #[inline]
+  pub const fn from_slice(v: &[f32])-> Self {
+    Vec3 {
+      x: v[0],
+      y: v[1],
+      z: v[2]
+    }
+  }
+
+  #[inline]
+  pub fn write_to_slice(self,slice: &mut [f32]) {
+    slice[0]=self.x;
+    slice[1]=self.y;
+    slice[2]=self.z;
   }
 }
 
