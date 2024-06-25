2103e7139a4de0bf2b0aaf7aca45d3c194105163
Nate River <trishiragolder69@outlook.com>
2024-06-25 19:54:43 +0530

    added constants

--- src/lib.rs
+++ src/lib.rs
@@ -13,6 +13,52 @@ pub struct Vec3 {
 
 
 impl Vec3 {
+  /// All zeroes.
+  pub const ZERO: Self = Self::splat(0.0);
+
+  /// All ones.
+  pub const ONE: Self = Self::splat(1.0);
+
+  /// All negative ones.
+  pub const NEG_ONE: Self = Self::splat(-1.0);
+
+  /// All `f32::MIN`.
+  pub const MIN: Self = Self::splat(f32::MIN);
+
+  /// All `f32::MAX`.
+  pub const MAX: Self = Self::splat(f32::MAX);
+
+  /// All `f32::NAN`.
+  pub const NAN: Self = Self::splat(f32::NAN);
+
+  /// All `f32::INFINITY`.
+  pub const INFINITY: Self = Self::splat(f32::INFINITY);
+
+  /// All `f32::NEG_INFINITY`.
+  pub const NEG_INFINITY: Self = Self::splat(f32::NEG_INFINITY);
+
+  /// A unit vector pointing along the positive X axis.
+  pub const X: Self = Self::new(1.0, 0.0, 0.0);
+
+  /// A unit vector pointing along the positive Y axis.
+  pub const Y: Self = Self::new(0.0, 1.0, 0.0);
+
+  /// A unit vector pointing along the positive Z axis.
+  pub const Z: Self = Self::new(0.0, 0.0, 1.0);
+
+  /// A unit vector pointing along the negative X axis.
+  pub const NEG_X: Self = Self::new(-1.0, 0.0, 0.0);
+
+  /// A unit vector pointing along the negative Y axis.
+  pub const NEG_Y: Self = Self::new(0.0, -1.0, 0.0);
+
+  /// A unit vector pointing along the negative Z axis.
+  pub const NEG_Z: Self = Self::new(0.0, 0.0, -1.0);
+
+  /// The unit axes.
+  pub const AXES: [Self; 3] = [Self::X, Self::Y, Self::Z];
+
+
   #[inline(always)]
   pub const fn new(x: f32,y: f32,z: f32)-> Self {
     Vec3 { x,y,z }
